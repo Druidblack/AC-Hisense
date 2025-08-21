9a36a0b6e0e71f01b8c4140b53a80b91a852826c
Artem Shevchenko <9002056+artshevchenko@users.noreply.github.com>
2025-08-21 15:44:31 +0300

    fix climate.py

--- components/ac_hi/climate.py
+++ components/ac_hi/climate.py
@@ -8,7 +8,7 @@ AUTO_LOAD = ["climate", "uart", "sensor", "switch"]
 ac_hi_ns = cg.esphome_ns.namespace("ac_hi")
 ACHIClimate = ac_hi_ns.class_("ACHIClimate", climate.Climate, cg.PollingComponent, uart.UARTDevice)
 
-# LED target switch class
+# Class for LED target switch (declared in C++)
 ACHILEDTargetSwitch = ac_hi_ns.class_("ACHILEDTargetSwitch", switch.Switch)
 
 CONF_ENABLE_PRESETS = "enable_presets"
@@ -19,7 +19,9 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(ACHIClimate),
     cv.Optional(CONF_ENABLE_PRESETS, default=True): cv.boolean,
     cv.Optional(CONF_PIPE_TEMPERATURE): sensor.sensor_schema(),
+    # Class-aware schema: lets us create switch via switch.new_switch(...)
     cv.Optional(CONF_LED_SWITCH): switch.switch_schema(
+        ACHILEDTargetSwitch,
         icon=ICON_LIGHTBULB,
         entity_category=ENTITY_CATEGORY_CONFIG,
     ),
@@ -29,8 +31,10 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
+
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_comp))
+
     cg.add(var.set_enable_presets(config[CONF_ENABLE_PRESETS]))
 
     if pipe := config.get(CONF_PIPE_TEMPERATURE):
@@ -39,6 +43,5 @@ async def to_code(config):
 
     # Optional LED target switch
     if led_sw_conf := config.get(CONF_LED_SWITCH):
-        led_sw = cg.new_Pvariable(await cg.generate_id())
-        await switch.register_switch(led_sw, led_sw_conf)
+        led_sw = await switch.new_switch(led_sw_conf)
         cg.add(var.set_led_switch(led_sw))
